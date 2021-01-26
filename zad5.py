25136d2590834220f9b7c5cb51e4e09c05bdda6c
Seewood <seweryn.zygas@student.up.krakow.pl>
2021-01-26 17:21:20 +0100

    zadania

--- zad5.py
+++ zad5.py
@@ -1,1 +1,15 @@
+import random
+
 names = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
+first = [x[0] for x in names]
+print(first)
+
+list_rand = []
+for x in range(5):
+    sub_list = []
+    for i in range(4):
+        sub_list += [random.randint(1, 10)]
+
+    list_rand.append(sub_list)
+
+print(list_rand)
