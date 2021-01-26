045f607558c654c1a303e40f69f645c1011f3a53
Seewood <seweryn.zygas@student.up.krakow.pl>
2021-01-26 20:12:00 +0100

    zadania

---
+++ zad11.py
@@ -0,0 +1,23 @@
+import os
+
+
+def rotatefile(count, file_size, file_name):
+    indX = 0
+    while indX < count:
+        with open(file_name, "a") as fd:
+            ##
+            fd.write("SOME text\n")
+            ##
+            if int(os.path.getsize(file_name)) > file_size:
+                indX += 1
+                file_name = "new_file"+str(indX)+".txt"
+
+
+if __name__ == "__main__":
+    count = 3
+    file_size = 100
+    file_name = "new_file.txt"
+    rotatefile(count, file_size, file_name)
+    print(os.stat('new_file.txt').st_size)
+    print(os.stat('new_file1.txt').st_size)
+    print(os.stat('new_file2.txt').st_size)
--- zad7.py
+++ zad7.py
@@ -1,3 +1,4 @@
+# Nie da się wywołać tej funkcji bez błędu, ponieważ argument *others jest przed age
 def person_print(name, last_name, *others, age):
     formatted_data = 'Imię: {}, nazwisko: {}, wiek: {}'.format(name, last_name, age)
     others_str = ' '
@@ -6,4 +7,14 @@ def person_print(name, last_name, *others, age):
     print(formatted_data + others_str)
 
 
-person_print("Seweryn", "Żygas", "Inne", 21)
+# własna funkcja
+
+def f(*args, **kwargs):
+    for index, val in kwargs.items():
+        print("%s == %s" % (index, val))
+
+    print("args: ", args)
+    print("kwargs: ", kwargs)
+
+
+f("something", 250, 'third',  first=1,  second="two", color="red")
