2ef4e440bdc76ed71672cba610e845bbadbaf235
Seewood <seweryn.zygas@student.up.krakow.pl>
2021-01-27 10:38:57 +0100

    up

--- zad2.py
+++ zad2.py
@@ -1,20 +1,25 @@
 y = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
 x = [[3, 2, 3], [2, 0, 2], [3, 0, 1], [3, 1, 0]]
-change_pos = 0
-for i in range(1, len(x)):
-    j = i
-    while j > 0:
-        for k in range(1, len(x[j])):
-            if (x[j - 1][k] != x[j][k]):
-                change_pos = k
-                break
+# change_pos = 0
 
-        if (x[j - 1][change_pos] > x[j][change_pos]):
-            swp_h = x[j]
-            x[j] = x[j - 1]
-            x[j - 1] = swp_h
-        else:
-            break
-        j -= 1
+y.sort(key=lambda e: (e[1], e[2]))
+print(y)
 
-print(x)
+
+# for i in range(1, len(x)):
+#     j = i
+#     while j > 0:
+#         for k in range(1, len(x[j])):
+#             if (x[j - 1][k] != x[j][k]):
+#                 change_pos = k
+#                 break
+#
+#         if (x[j - 1][change_pos] > x[j][change_pos]):
+#             swp_h = x[j]
+#             x[j] = x[j - 1]
+#             x[j - 1] = swp_h
+#         else:
+#             break
+#         j -= 1
+#
+# print(x)
