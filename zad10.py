93a79a0241ea6ee4888e81a9de89c3b61f9343a1
Seewood <seweryn.zygas@student.up.krakow.pl>
2022-01-17 18:07:14 +0100

    elo2

--- zad10.py
+++ zad10.py
@@ -2,7 +2,7 @@ import re
 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque venenatis nunc vel ultricies. " \
        "Morbii quis elit eu ipsum sodales pulvinar. Ut tempus eros ac facilisis bibendum. Sed tellus nulla, " \
        "euidfsdfssmod eget varius eu, tristique quis mauris. Phasellus sollicitudin pretium tellus et pretium. Vestibulum " \
-       "luctus velit nec tortor fringilla venenatis ut vitae diam. Nam aliquet iaculis purus et fringilla. Mauris " \
+       "luctus velit nec tortor frisdfsdfsdngilla venenatis ut vitae diam. Nam aliquet iaculis purus et fringilla. Mauris " \
        "pulvinar cursus lorem sit amet feugiat. Quisque ac mollis ex. "
 
 print(re.findall(r"\b\w{4,}\b", text))
